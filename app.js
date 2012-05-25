abde766474043da201aa43f88d34f32d40a70c01
Riccardo Strobbia <riccardo@robanostra.com>
2012-05-25 10:41:21 +0200

    port

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.get('/logout', function(req, res){
 });
 
 //go
-app.listen(3000, function(){
+app.listen( process.env['app_port'] || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
