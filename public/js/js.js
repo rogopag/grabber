563b4bb113124e3cdb49a7283dbbfdd655a730bd
Riccardo Strobbia <riccardo@robanostra.com>
2012-06-03 01:04:58 +0200

    GUI and stuff

--- public/js/js.js
+++ public/js/js.js
@@ -11,12 +11,30 @@ function main()
 		init:function()
 		{
 			self = this;
+			self.populate();
 			self.addNewGrabControls();
 			self.socket_listen();
 		},
-		actionGrabber:function( action, data )
+		socketEmit:function( action, data )
 		{
 			self.socket.emit(action, data);
+		},
+		populate:function()
+		{
+			if( $("#appform").is('div') )
+			{
+				self.socketEmit('populate', {action:'populate'} );
+				self.socket.on('populate', function(data){
+					$.each(data, function( key, value){
+						self.printBox(value.name, value.url, value.status != 'created' );
+						self.updateInputs(value);
+						var button = self.printButton(value.name, 'start', 'stop', value.status != 'created' && value.status != 'stopped' );
+						$("#"+value.name+"_create_grabber").after(button);
+						self.messages(value.name, "Grabber object on "+value.status)
+						self.socket.emit('socket_restart', self.fillDataToSend( button ) );
+					})
+				});
+			}
 		},
 		fillDataToSend:function( el )
 		{
@@ -28,60 +46,112 @@ function main()
 			});
 			return data;
 		},
-		printButton:function( name, callback, action_one, action_two )
+		printButton:function( name, action_one, action_two, selected )
 		{	
+			var sel = ( selected ) ? true : false;
 			var $button = $.ninja.button({
-				html: action_one+' '+name 
+				html: ( sel ) ? action_two +' '+name : action_one +' '+name,
+				select:sel
 			}).select(function () {
 
 				$(this).text(action_two+' '+name);
-				self.fillDataToSend( $(this));
-				//callback( action_one, self.fillDataToSend( $(this) ) );
+				self.fillDataToSend( $(this) );
+				self.socketEmit( action_one, self.fillDataToSend( $(this) ) );
 
 			}).deselect(function () {
 				$(this).text(action_one+' '+name);
-				self.fillDataToSend( $(this) );
-				//callback( action_two, self.fillDataToSend( $(this) ) );
+				self.socketEmit( action_two, self.fillDataToSend( $(this) ) );
+				$(this).removeClass('nui-slc');
 				}),
 				$buttonSelect = $.ninja.button({
 					html: 'Selected',
 					select: true
 				});
 				$button.attr('id', name+'_'+action_one+'_grabber');
+				$button.css('display', 'none');
+				$button.fadeIn(300, function(){
+					
+				});
 				return $button;
 			},
-			printBox:function( name )
+			printBox:function( name, url, selected )
 			{
-				var container = $('<div class="grabbers_box" id="'+name+'_box"/>') 
+				var container = $('<div class="grabbers_box" id="'+name+'_box"/>')
+				, h3 =  $('<h3 class="grabbers_box_title" id="'+name+'_title"/>')
 				, buttonDiv = $('<div class="grabber-button" id="'+name+'_button"></div>')
 				, messages = $('<div class="messages" id="'+name+'_messages"></div>')
-				, button;
-				button = self.printButton( name, self.actionGrabber, 'create', 'destroy' );
+				, span = $('<span class="grabbers_url" />')
+				, button
+				, sel = ( selected ) ? true : false;
+				button = self.printButton( name, 'create', 'destroy', sel );
 				self.boxFields( name,  container );
 				buttonDiv.append(button);
-				container.append(buttonDiv, messages);
+				h3.text(name);
+				span.text(" url: "+url);
+				h3.append(span);
+				container.append(h3, buttonDiv, messages);
 				$("#boxes").append( container );
+			},
+			updateInputs:function( d )
+			{
+				var data = d;
+				$.each(data, function(key, value){
+					if( key != 'message' )
+					{
+						$("input#"+data.name+"_"+key).val(value);
+					}
+				});
 			},
 			socket_listen:function()
 			{
 				self.socket.on('created', function (data){
-					console.log(data);
-					self.messages(data.message);
+					if( data )
+					{
+						var button = self.printButton(data.name, 'start', 'stop');
+						$("#"+data.name+"_create_grabber").after(button);
+						self.messages(data.name, data.message);
+						self.updateInputs(data);
+					}
+					
 				});
-				self.socket.on('news', function (data) {
-					self.messages(data.message);
+				self.socket.on('started', function (data) 
+				{
+					if(data)
+					{
+						self.messages(data.name, data.message);
+						self.updateInputs(data);
+					}
+				});
+				self.socket.on('stopped', function (data) {
+					if(data)
+					{
+						self.messages(data.name, data.message);
+						self.updateInputs(data);
+					}
 				});
 				
+				self.socket.on('news', function (data) {
+					self.messages(data.name, data.message);
+				});
 			},
-			messages:function(message)
+			messages:function(name, message)
 			{
-				var p = $('<p class="message" />')
+				var p = $('<p class="message" />');
+				p.hide();
 				p.text(message);
-				$("#messages").append(p);
+				$("#"+name+"_messages").append(p).fadeIn(200, function(){
+					if( $(this).children('p').length > 3 )
+					{
+						$(this).children('p').eq(1).fadeOut(100, function(){
+							console.log( $(this).attr('class'), 'removed')
+						});
+					}
+				});
+				p.fadeIn(200);
 			},
 			boxFields:function(name, container)
 			{
-				var fields = ['name', 'url', 'status', 'timestamp'];
+				var fields = ['index', 'name', 'url', 'status', 'timestamp', 'file'];
 				$.each(fields, function(key, value){
 					var field = $('<input type="hidden" name="'+name+'_'+value+'" value="" id="'+name+'_'+value+'">');
 					container.append(field);
@@ -99,7 +169,7 @@ function main()
 			
 				if( url && name && !$('#'+name+'_box').is('div') )
 				{
-					self.printBox(name);
+					self.printBox(name, url);
 					$('#'+name+'_name').val(name);
 					$('#'+name+'_url').val(url);
 				}
